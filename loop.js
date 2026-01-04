1b8111955fefed2917b425cfee992cd5f2bbaedd
coder-utshab <ghoshutshob470@gmail.com>
2026-01-04 17:40:16 +0530

    up

--- loop.js
+++ loop.js
@@ -1,2 +1,13 @@
 // const : value will not change
 // let : value can change
+
+const fruits = ['oreange', 'apple', 'banana', 'jackfruit', 'watermelon'];
+
+// for(){
+
+// }
+
+for (const fruit of fruits){
+console.log(fruit);
+console.log('i want to eat');
+}
---
+++ while.js
@@ -0,0 +1,15 @@
+const number = 7;
+if (number < 10) {
+    console.log('choto ekta number paisi')
+}
+console.log('something new is cooking')
+
+
+// --------------while ---------loop-----------
+
+// condition true hoyla loop bar bar cholba 
+// ar jodi condition false hoy tahola okhanay off hoya jaba
+const num = 0;
+while(num < 4){
+    console.log('loopingggggggggg')
+}
