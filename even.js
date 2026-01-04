72a836c8145afe23e64614275e87e6223143afe8
coder-utshab <ghoshutshob470@gmail.com>
2026-01-04 18:11:29 +0530

    up

--- even.js
+++ even.js
@@ -7,3 +7,14 @@ while(num <= 10){
     }
     num++;
 }
+
+
+// even
+for(let i = 0; i <= 20; i=i+2){
+    console.log(i)
+}
+
+// odd
+for(let i = 1; i <= 20; i=i+2){
+    console.log(i);
+}
---
+++ for.js
@@ -0,0 +1,13 @@
+// ------------------ while loop----------------
+
+           
+let num = 0;  //loop variable              
+while(num < 5) {  //loop condition
+    console.log(num); //loop statement
+    num++    //loop  variable change
+}
+
+
+for( let i = 1; i < 50; i++){
+    console.log(i);
+}
