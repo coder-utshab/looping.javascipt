29345b6d225cfae987f81d394bb6d121a6dd6efb
coder-utshab <ghoshutshob470@gmail.com>
2026-01-05 19:10:10 +0530

    up

---
+++ string.js
@@ -0,0 +1,11 @@
+const country = 'bangladesh';
+const division = "magura";
+console.log(country);
+
+
+
+
+const numbers = [54, 98, 45, 35,42];
+console.log(numbers)
+numbers[1] = 11;
+console.log(numbers)
