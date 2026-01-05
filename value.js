ced2440926e0d4cc71c5b74de41fe8ec2b2f2bdb
coder-utshab <ghoshutshob470@gmail.com>
2026-01-06 00:40:38 +0530

    up

---
+++ value.js
@@ -0,0 +1,11 @@
+const person = {
+name: 'sodor uddin',
+age: 25,
+profession: 'devoloper',
+salary: 25000,
+married: true,
+'fav places': ['bandarban', 'saintmartin', 'kuakata']
+}
+
+person.salary = 30000;
+console.log(person)
