5e8cf4369a293b7524123d7ce4ccaa3b2d19ae00
coder-utshab <ghoshutshob470@gmail.com>
2026-01-05 19:40:42 +0530

    ut

---
+++ case.js
@@ -0,0 +1,18 @@
+const school = 'bunagati degree colleage';
+
+console.log(school);
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
+
+//uppercase:ABCD EFG
+//lowercase: abcd efg
+
+const subject = 'chemistry';
+const book = 'Chemistry';
+
+if(subject.toLowerCase() === book.toLowerCase()){
+    console.log('I am a readding book i studey in parul university');
+}
+else{
+    console.log('i am from bangladesh & i am a student');
+}
---
+++ slice.js
@@ -0,0 +1,7 @@
+const address = 'utshabkumar';
+const part = address.slice(2, 5);
+console.log(part);
+
+//output ashba
+//-------------------
+//sha
