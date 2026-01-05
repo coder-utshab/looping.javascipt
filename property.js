5950e3f1d4cfd7a5bce28031090462fd66c75a2f
coder-utshab <ghoshutshob470@gmail.com>
2026-01-05 22:50:08 +0530

    up

---
+++ property.js
@@ -0,0 +1,14 @@
+//akta object declear korar jonno
+
+const person = {
+    name: 'sajib biswas',
+    age: 25,
+    profession:'devoloper',
+    salary:25000,
+    married: false,
+}
+// console.log(person)
+//kono akta object ar property dakta chayla
+console.log(person.profession)
+const income = person.salary;
+console.log(income)
